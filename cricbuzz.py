9b78748ddb0c05b9551fd19443b721ca9eb70e68
Himanshu Ladia <hladia199811@gmail.com>
2017-12-18 14:28:30 +0530

    Fix scores not displaying

--- cricbuzz.py
+++ cricbuzz.py
@@ -8,22 +8,27 @@ soup = BeautifulSoup(source, 'lxml')
 parent = soup.find('div', class_='cb-col cb-col-25 cb-mtch-blk')
 
 # print(parent.prettify())
+# Use tag.text to get text and tag.get_text() to get None if text d.n.e
+# Use tag['attribute'] to get attribute and tag.get('attribute') to get None if attribute d.n.e
 
-title = parent.a['title']
+title = parent.find('a').get('title')
 print(title)
 
-batting = parent.a.find('div', class_='cb-hmscg-bat-txt cb-ovr-flo ')
-bowling = parent.a.find('div', class_='cb-hmscg-bwl-txt ')
+batting = parent.find('a').find('div', class_='cb-hmscg-bat-txt cb-ovr-flo ')
+bowling = parent.find('a').find('div', class_='cb-hmscg-bwl-txt ')
 
-batting_team = batting.find('div', class_='cb-ovr-flo cb-hmscg-tm-nm').text
-bowling_team = bowling.find('div', class_='cb-ovr-flo cb-hmscg-tm-nm').text
+batting_team = batting.find_all('div')[0].get_text()
+batting_score = batting.find_all('div')[1].get_text()
 
-batting_score = batting.find('div', class_='cb-ovr-flo').text
-bowling_score = bowling.find('div', class_='cb-ovr-flo').text
+bowling_team = bowling.find_all('div')[0].get_text()
+bowling_score = bowling.find_all('div')[1].get_text()
 
 print(f'{batting_team}	{batting_score}')
 print(f'{bowling_team}	{bowling_score}')
 
-status = parent.find('div', class_=' cb-ovr-flo cb-text-live').text
-
-print(status)
+try:
+    status = parent.find('div', class_=' cb-ovr-flo cb-text-live').text
+    print("Match Live!\n" + status)
+except:
+    status = parent.find('div', class_=' cb-ovr-flo cb-text-complete').text
+    print("Match Complete!\n" + status)
